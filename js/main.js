ccaf10c4876157dcbc94aec498a992ba84d629ec
Blate00 <da.coo@duocuc.cl>
2023-04-01 20:27:47 -0300

    js agregado

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function login(){
+    var user, password
+
+    user = document.getElementById("user").value;
+    password = document.getElementById("password").value;
+
+    if(user == "machuca-te" && password == 1234){
+        window.location = "index.html";
+    }else{
+        alert("Datos incorrectos")
+    }
+
+}
